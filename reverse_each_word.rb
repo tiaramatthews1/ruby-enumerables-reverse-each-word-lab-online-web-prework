e6f6c4eafbf77afa6e0ed687ecbe517b5ae2619b
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-08 04:20:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-def reverse_each_work(String)
+def reverse_each_word(String)
   string_array = string.split()
   new_string_array = array_string.collect {word| word. reverse}
   new_string_array.join(' ')
